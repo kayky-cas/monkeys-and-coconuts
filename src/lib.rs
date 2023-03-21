e4341b5a5b0995a2ee3e278be667d5de32b177ed
Kayky Belleboni Casagrande <kbelleboni@gmail.com>
2023-03-20 22:40:13 -0300

    ????

--- src/lib.rs
+++ src/lib.rs
@@ -1,44 +1,68 @@
-use std::{cell::RefCell, rc::Rc};
+use std::{cell::RefCell, rc::Rc, str::FromStr};
 
 pub enum CoconutType {
     Even,
     Odd,
 }
 
+#[derive(Debug)]
 pub struct Monkey {
     even: usize,
     odd: usize,
-    even_coconuts: Vec<usize>,
-    odd_coconuts: Vec<usize>,
+    evens: usize,
+    odds: usize,
 }
 
-pub struct CoconoutGame {
+impl FromStr for Monkey {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let numbers: Vec<_> = s
+            .trim()
+            .split(' ')
+            .map(|s| s.parse::<usize>().ok())
+            .flatten()
+            .skip(1)
+            .collect();
+
+        let even = numbers[0];
+        let odd = numbers[1];
+
+        let coconuts = &numbers[1..];
+        let evens = coconuts.iter().filter(|&x| x % 2 == 0).count();
+        let odds = coconuts.len() - evens;
+
+        Ok(Self {
+            even,
+            odd,
+            evens,
+            odds,
+        })
+    }
+}
+
+type MonkeyRef = Rc<RefCell<Monkey>>;
+
+pub struct CoconutGame {
     rounds: i32,
-    monkeys: Vec<Rc<RefCell<Monkey>>>,
+    monkeys: Vec<MonkeyRef>,
 }
 
-impl CoconoutGame {
-    fn pass_coconuts(
-        &self,
-        monkey: Rc<RefCell<Monkey>>,
-        other: Rc<RefCell<Monkey>>,
-        coconut_type: CoconutType,
-    ) {
+impl CoconutGame {
+    fn pass_coconuts(&self, monkey: MonkeyRef, other: MonkeyRef, coconut_type: CoconutType) {
         match coconut_type {
             CoconutType::Even => {
-                while let Some(coconut) = monkey.borrow_mut().even_coconuts.pop() {
-                    other.borrow_mut().even_coconuts.push(coconut);
-                }
+                other.borrow_mut().evens += monkey.borrow_mut().evens;
+                monkey.borrow_mut().evens = 0;
             }
             CoconutType::Odd => {
-                while let Some(coconut) = monkey.borrow_mut().odd_coconuts.pop() {
-                    other.borrow_mut().odd_coconuts.push(coconut);
-                }
+                other.borrow_mut().odds += monkey.borrow_mut().odds;
+                monkey.borrow_mut().odds = 0;
             }
         }
     }
 
-    pub fn play(&mut self) {
+    pub fn play(&mut self) -> usize {
         let mut rounds = self.rounds;
 
         while rounds > 0 {
@@ -47,12 +71,56 @@ impl CoconoutGame {
             for i in 0..self.monkeys.len() {
                 let monkey = self.monkeys[i].clone();
 
-                let even_monkey = self.monkeys[monkey.borrow().even].clone();
-                let odd_monkey = self.monkeys[monkey.borrow().odd].clone();
+                let even_monkey = self.monkeys[monkey.borrow_mut().even].clone();
+                let odd_monkey = self.monkeys[monkey.borrow_mut().odd].clone();
 
                 self.pass_coconuts(monkey.clone(), even_monkey, CoconutType::Even);
                 self.pass_coconuts(monkey, odd_monkey, CoconutType::Even);
             }
         }
+
+        let first_monkey = self.monkeys[0].clone();
+
+        let mut winner = 0;
+        let mut max = first_monkey.borrow().evens + first_monkey.clone().borrow().odds;
+
+        for (index, monkey) in self.monkeys.iter().enumerate().skip(1) {
+            let monkey = monkey.clone();
+
+            let total = monkey.borrow().evens + monkey.clone().borrow().odds;
+
+            if total > max {
+                max = total;
+                winner = index;
+            }
+        }
+
+        return winner;
+    }
+}
+
+impl FromStr for CoconutGame {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let rounds = s
+            .lines()
+            .nth(0)
+            .ok_or(anyhow::anyhow!("Round not found at the top of the file!"))?
+            .split(' ')
+            .map(|s| s.parse::<i32>().ok())
+            .flatten()
+            .nth(0)
+            .unwrap();
+
+        let monkeys = s
+            .lines()
+            .skip(1)
+            .map(|line| line.parse::<Monkey>().ok())
+            .flatten()
+            .map(|monkey| Rc::new(RefCell::new(monkey)))
+            .collect();
+
+        Ok(Self { rounds, monkeys })
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,60 @@
-fn main() {}
+use std::{env, fs, thread};
+
+use anyhow::Error;
+use monkeys_and_coconuts::CoconutGame;
+
+fn game_from_especific_file(input: &str) -> Result<(), Error> {
+    let buffer = fs::read_to_string(input)?;
+
+    let mut game: CoconutGame = buffer.parse()?;
+
+    println!("{} winner: {}", input, game.play());
+
+    Ok(())
+}
+
+fn game_from_folder(folder: &str) -> Result<(), Error> {
+    let dir = fs::read_dir(folder)?;
+
+    let mut threads = Vec::new();
+
+    for file in dir.into_iter().map(|f| f.ok()).flatten() {
+        let tr = thread::spawn(move || {
+            let _ = game_from_especific_file(&file.path().to_str().unwrap());
+        });
+
+        threads.push(tr);
+    }
+
+    loop {
+        if threads.iter().filter(|tr| !tr.is_finished()).count() == 0 {
+            break;
+        }
+    }
+
+    Ok(())
+}
+
+fn main() -> Result<(), Error> {
+    let _program_name = env::args()
+        .nth(0)
+        .ok_or(anyhow::anyhow!("Invalid program name"))?;
+
+    let args: Vec<_> = env::args().skip(1).collect();
+
+    if args.is_empty() {
+        game_from_folder("./casos")?;
+
+        return Ok(());
+        //return Err(anyhow::anyhow!(
+        //    "{} needs at least 1 argument to execute.",
+        //    program_name
+        //));
+    }
+
+    let input_name = &args[0];
+
+    game_from_especific_file(input_name)?;
+
+    Ok(())
+}
